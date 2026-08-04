aba8b905def13f49cd242bea18b2db0479ed3c17
kpmisthah <kpmisthah6@gmail.com>
2026-08-04 18:07:04 +0530

    feat: add scroll observer for animations

---
+++ src/modules/user/components/ScrollObserver.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from 'react';
+import { useScrollAnimation } from "@/modules/user/hooks/useScrollAnimation";
+
+export default function ScrollObserver() {
+    useScrollAnimation();
+    return null;
+}
